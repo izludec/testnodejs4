3c7eb560a3ce659bef21c7db617b481a571e8592
Kirill Izludec <izludec@hotmail.com>
2016-11-10 15:39:22 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express()
+var url = require('url')
+var mass = [];
+ 
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+      api_key: "2da04588e0361a239d9d7a45e049584f",
+      secret: "920250461bcaee27"
+    };
+
+app.set('view engine', 'ejs');
+
+
+app.get('/*',function (req, res) { 
+    Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+        flickr.photos.search({
+            tags : String(req.url).split("/")[1],
+            page: String(req.url).split("/")[2],
+            per_page: 10
+        }, function(err, result) {
+            for(var i in result['photos']['photo']){
+            mass.push("https://c1.staticflickr.com/"
+            +result['photos']['photo'][i]["farm"]
+            +"/"+result['photos']['photo'][i]["server"]
+            +"/"+result['photos']['photo'][i]["id"]
+            +"_"+result['photos']['photo'][i]["secret"]
+            +"_b.jpg")}
+           
+            res.render('search',
+        	{photos:mass});
+        	mass = []
+        });
+    });
+    
+    
+    
+    
+})
+
+
+
+
+    app.listen(process.env.PORT || 8080,function(){
+      console.log("Started 8080")
+    })
